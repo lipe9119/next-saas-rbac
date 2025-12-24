b2cfb1e3420586d92cda8e91aec3be76a149f6da
Felipe Dornelles <lipe.dornelles@hotmail.com>
2025-12-23 23:38:58 -0300

    feat: add initial implementation of multi-tenant SaaS with RBAC
    - Introduced authentication features including email/password and GitHub login.
    - Implemented organization management with CRUD operations.
    - Added invite system for new members and role management.
    - Developed project management capabilities within organizations.
    - Established billing details retrieval for organizations.
    - Defined roles and permissions for users, including Admin, Member, and Billing roles.
    - Created schemas for user, organization, project, and permissions using Zod.
    - Integrated CASL for role-based access control.
    - Set up TypeScript configuration for Node.js environment.
    - Added README documentation outlining project features and RBAC structure.

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,1 +1,47 @@
-const teste = "sdfsa";
+import type { CreateAbility, MongoAbility } from "@casl/ability";
+import { AbilityBuilder, createMongoAbility } from "@casl/ability";
+import z from "zod";
+import type { User } from "./models/user";
+import { permissions } from "./permissions";
+import { billingSubject } from "./subjects/billing";
+import { inviteSubject } from "./subjects/invite";
+import { organizationSubject } from "./subjects/organization";
+import { projectSubject } from "./subjects/project";
+import { userSubject } from "./subjects/user";
+
+export * from "./models/organization";
+export * from "./models/project";
+export * from "./models/user";
+
+const appAbilitiesSchemas = z.union([
+  projectSubject,
+  userSubject,
+  organizationSubject,
+  inviteSubject,
+  billingSubject,
+  z.tuple([z.literal("manage"), z.literal("all")]),
+]);
+
+type AppAbilities = z.infer<typeof appAbilitiesSchemas>;
+
+export type AppAbility = MongoAbility<AppAbilities>;
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>;
+
+export function defineAbilityFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility);
+
+  const { role } = user;
+  if (typeof permissions[role] !== "function") {
+    throw new Error(`Permissions for role ${role} not found`);
+  }
+
+  permissions[role](user, builder);
+
+  const ability = builder.build({
+    detectSubjectType: (subject) => {
+      return subject.__typename;
+    },
+  });
+
+  return ability;
+}
